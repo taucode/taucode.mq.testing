21a6196d865b420d6f1f90ee0331e0db0cdaa104
Andrey Kovalenko <ak@taucode.com>
2023-02-06 12:54:32 +0200

    Winter resurrection - 2023.

--- pre-deploy.cmd
+++ pre-deploy.cmd
@@ -1,12 +1,9 @@
 dotnet restore
 
-dotnet clean --configuration Debug
-dotnet clean --configuration Release
+dotnet build TauCode.Mq.Testing.sln -c Debug
+dotnet build TauCode.Mq.Testing.sln -c Release
 
-dotnet build --configuration Debug
-dotnet build --configuration Release
-
-dotnet test -c Debug .\test\TauCode.Mq.Testing.Tests\TauCode.Mq.Testing.Tests.csproj
-dotnet test -c Release .\test\TauCode.Mq.Testing.Tests\TauCode.Mq.Testing.Tests.csproj
+dotnet test TauCode.Mq.Testing.sln -c Debug
+dotnet test TauCode.Mq.Testing.sln -c Release
 
 nuget pack nuget\TauCode.Mq.Testing.nuspec
